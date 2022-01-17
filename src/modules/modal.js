1542172aad4538619bec3823bdca7ef446c6a8ae
Iuliia Emelianova <julia03951@gmail.com>
2022-01-17 10:40:27 +0300

    отменено действие по умолчанию для можальных окон

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -5,12 +5,18 @@ const modal = () => {
 
   document.addEventListener('click', (e) => {
     if (e.target.matches('#header .button a')) {
+      e.preventDefault()
+      modalHeader.style.display = 'flex'
+      overlay.style.display = 'block'
+    } else if (e.target.matches('.phone-icon')) {
+      e.preventDefault()
       modalHeader.style.display = 'flex'
       overlay.style.display = 'block'
     } else if (e.target.matches('.header-modal__close')) {
       modalHeader.style.display = 'none'
       overlay.style.display = 'none'
     } else if (e.target.matches('.service-button a')) {
+      e.preventDefault()
       modalServices.style.display = 'flex'
       overlay.style.display = 'block'
     } else if (e.target.matches('.services-modal__close')) {
