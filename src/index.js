286c0705ab984321d80f3b9839a4766814c44570
Iuliia Emelianova <julia03951@gmail.com>
2022-01-17 23:17:57 +0300

    Создан слайдер для секции benefits

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import modal from './modules/modal'
 import scrollUp from './modules/scrollUp'
 import slider from './modules/slider'
+import sliderBenefits from './modules/slider-benefits'
 
 modal()
 scrollUp()
 slider()
+sliderBenefits()
--- src/modules/slider-benefits.js
+++ src/modules/slider-benefits.js
@@ -1,28 +1,22 @@
-const slider = () => {
-  const services = document.querySelector('#services')
-  const sliderContainer = services.querySelector('.row')
-  const sliderItems = sliderContainer.querySelectorAll('div')
+const sliderBenefits = () => {
+  const benefits = document.querySelector('#benefits')
+  const benefitsInner = benefits.querySelector('.benefits-wrap')
+  const benefitsItem = benefits.querySelectorAll('.benefits__item')
 
   let offsetWidth
-  const arrItems = []
   let currentSlide = 0
-
-  sliderItems.forEach((item) => {
-    if (item.className.match(/col/gi)) {
-      arrItems.push(item)
-    }
-  })
 
   const getBlockHidden = (arr) => {
     arr.forEach((item, index) => {
       offsetWidth = document.documentElement.offsetWidth
       if (offsetWidth > 576) {
-        if (index === 0 || index === 1) {
+        if (index >= 0 && index <= 2) {
           item.style.display = 'block'
         } else {
           item.style.display = 'none'
         }
       } else if (offsetWidth < 576) {
+        benefitsInner.style.justifyContent = 'center'
         if (index === 0) {
           item.style.display = 'block'
         } else {
@@ -35,6 +29,8 @@ const slider = () => {
   const prevSlide = (elems, index) => {
     offsetWidth = document.documentElement.offsetWidth
     if (offsetWidth > 576) {
+      elems[index].style.display = 'none'
+      index++
       elems[index].style.display = 'none'
       index++
       elems[index].style.display = 'none'
@@ -52,46 +48,54 @@ const slider = () => {
       elems[index].style.display = 'block'
       index++
       elems[index].style.display = 'block'
+      index++
+      elems[index].style.display = 'block'
     } else if (offsetWidth < 576) {
       elems[index].style.display = 'block'
       index--
     }
   }
-  
-  sliderContainer.addEventListener('click', (e) => {
+
+  benefits.addEventListener('click', (e) => {
+    
     offsetWidth = document.documentElement.offsetWidth
     e.preventDefault()
-    prevSlide(arrItems, currentSlide)
+    prevSlide(benefitsItem, currentSlide)
     
-    if(e.target.closest('.services__arrow--left')) {
+    if(e.target.closest('.benefits__arrow--left')) {
       currentSlide--
-    } else if (e.target.closest('.services__arrow--right')) {
+    } else if (e.target.closest('.benefits__arrow--right')) {
       currentSlide++
     }
 
-    if (currentSlide >= arrItems.length-1) {
-      currentSlide = 0
+    if (offsetWidth > 576) {
+      if (currentSlide >= benefitsItem.length-2) {
+        currentSlide = 0
+      }
+    } else if (offsetWidth < 576) {
+      if (currentSlide >= benefitsItem.length) {
+        currentSlide = 0
+      }
     }
+
 
     if (offsetWidth > 576) {
       if (currentSlide < 0) {
-        currentSlide = arrItems.length-2
+        currentSlide = benefitsItem.length-3
       }
     } else if (offsetWidth < 576) {
       if (currentSlide < 0) {
-        currentSlide = arrItems.length-1
+        currentSlide = benefitsItem.length-1
       }
     }
 
-    nextSlide(arrItems, currentSlide)
+    nextSlide(benefitsItem, currentSlide)
   })
 
-  getBlockHidden(arrItems)
+  getBlockHidden(benefitsItem)
   window.addEventListener('resize', () => {
-    getBlockHidden(arrItems)
+    getBlockHidden(benefitsItem)
   })
 }
 
-export default slider
-
-
+export default sliderBenefits
