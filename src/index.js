4e55deed8efb916d2b50507684593428e2c2a30e
Iuliia Emelianova <julia03951@gmail.com>
2022-01-18 17:13:29 +0300

    Реализован таймер в обоих блоках order

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import scrollUp from './modules/scrollUp'
 import slider from './modules/slider'
 import sliderBenefits from './modules/slider-benefits'
 import modalImg from './modules/modal-img'
+import timer from './modules/timer'
 
 modal()
 scrollUp()
 slider()
 sliderBenefits()
 modalImg()
+timer('30 januar 2022')
