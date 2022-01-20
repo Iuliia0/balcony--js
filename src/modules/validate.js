94bbc36b3c797407539f60fa40f3bc2573a3d7a4
Iuliia Emelianova <julia03951@gmail.com>
2022-01-20 19:50:58 +0300

    Исправлена валидация формы

---
+++ src/modules/validate.js
@@ -0,0 +1,30 @@
+const validate = () => {
+  const phones = document.querySelectorAll('input[name="phone"]')
+  const names = document.querySelectorAll('input[name="fio"]')
+
+  
+
+  const regText = /[\d\=\+\)\(\*\?\:\;\%\$\&\#\№\^\_\@\'\"\<\>\,\.\!\~\`\-]+/gi
+  const regTel =  /[a-zа-я\=\)\(\*\?\:\;\%\$\&\#\№\^\_\@\'\"\<\>\,\.\!\~\`\-]/g
+
+  phones.forEach(phone => {
+    phone.maxLength = 16
+  })
+
+  const getText = (arr, reg) => {
+    
+    arr.forEach(item => {
+      item.addEventListener('input', (e) => {
+        e.target.value = e.target.value.replace(reg, '')
+      })
+    })
+
+  }
+
+  getText(names, regText)
+  getText(phones, regTel)
+
+
+}
+
+export default validate
